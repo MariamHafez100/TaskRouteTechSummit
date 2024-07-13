a8e621556909e4621b96f5a4891a860fe2ace423
MariamHafez100 <mariamhafez401@gmail.com>
2024-07-14 00:02:26 +0300

    done completely with unit testing

--- lib/ui/utils/my_colors.dart
+++ lib/ui/utils/my_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const primaryColor = Color(0xff004182);
   static const darkPrimaryColor = Color(0xff004182);
   static const whiteColor = Color(0xffffffff);
-  static Color babyblueColor = Color(0xFF6E92B4);
+  static Color babyblueColor = Color(0xFF86B3DE);
   static const blackColor = Color(0xFF000000);
   static Color greyColor = Color(0xff808080);
   static Color blueGreyColor = Color(0xFF607D8B);
